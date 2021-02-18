c05c1d2962d7da934bf1a2441ad6cea734741b4c
TJ DeVries <devries.timothyj@gmail.com>
2021-02-18 15:27:05 -0500

    Auto pull submodule

--- config.sh
+++ config.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# Grab backwards-cpp and update
+git submodule update --init --recursive
+
 cmake -B build -S . \
       -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
       -DPATH_TO_LLVM=$PATH_TO_LLVM
