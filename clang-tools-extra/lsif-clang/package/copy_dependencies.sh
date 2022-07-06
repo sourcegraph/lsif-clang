be6a3a11aa2636073b6676b845d0d917afa4d32e
Varun Gandhi <varun.gandhi@sourcegraph.com>
2022-07-06 14:53:16 +0000

    packaging: Exclude librt and libpthread from bundle.
    They link in private symbols from Glibc, leading to linker
    errors when trying to use lsif-clang across Ubuntu versions.
    
    Error with libpthread:
      lsif-clang: symbol lookup error: /home/varun/lsif-clang/build/bundled/lsif-clang-34e509b99/libpthread.so.0: undefined symbol: __libc_vfork, version GLIBC_PRIVATE
    
    Error with librt:
      lsif-clang: symbol lookup error: /home/varun/lsif-clang/build/bundled/lsif-clang-34e509b99/librt.so.1: undefined symbol: __clock_nanosleep, version GLIBC_PRIVATE

--- clang-tools-extra/lsif-clang/package/copy_dependencies.sh
+++ clang-tools-extra/lsif-clang/package/copy_dependencies.sh
@@ -10,6 +10,6 @@ fi
 binary="$1"
 output="$2"
 
-for lib in $(ldd "$binary" | cut -d ">" -f 2 | awk '{print $1}' | grep -vE "(linux-vdso\.|ld-linux-x86-64\.|libc\.)"); do
+for lib in $(ldd "$binary" | cut -d ">" -f 2 | awk '{print $1}' | grep -vE "(linux-vdso\.|ld-linux-x86-64\.|libc\.|librt\.|libpthread\.)"); do
     cp --verbose "$lib" "$output/"
 done
