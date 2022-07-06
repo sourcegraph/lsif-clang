3e7c18012687310da4beb59953f22a66b83fbb1c
Varun Gandhi <varun.gandhi@sourcegraph.com>
2022-07-06 21:41:26 +0800

    build: Put artifacts under build/ and optionally copy them out.

--- clang-tools-extra/lsif-clang/package/build.sh
+++ clang-tools-extra/lsif-clang/package/build.sh
@@ -20,7 +20,7 @@ dockercleanup
 cd "$(dirname "${BASH_SOURCE[0]}")"
 SCRIPT_DIR="$(pwd)"
 ROOT_DIR="$SCRIPT_DIR/../../.."
-GIT_COMMIT_SHA="$(git rev-parse HEAD)"
+GIT_COMMIT_SHA="$(git rev-parse --short HEAD)"
 
 # Build
 cd "$ROOT_DIR"
@@ -32,8 +32,8 @@ echo "[Starting temporary container: $CONTAINER_NAME]"
 docker create --name "$CONTAINER_NAME" "$IMAGE_NAME"
 
 OUTPUT_TARBALL_BASENAME="lsif-clang-${GIT_COMMIT_SHA}"
-OUTPUT_DIR="${ROOT_DIR}/${OUTPUT_TARBALL_BASENAME}"
-TARBALL_NAME="${OUTPUT_TARBALL_BASENAME}.tar.gz"
+OUTPUT_DIR="${ROOT_DIR}/build/bundled/${OUTPUT_TARBALL_BASENAME}"
+TARBALL_PATH="${ROOT_DIR}/build/bundled/${OUTPUT_TARBALL_BASENAME}.tar.gz"
 
 # Get the files we need out of the image
 echo "[Extracting needed files]"
@@ -49,5 +49,13 @@ cp "$SCRIPT_DIR/lsif_clang_shim.sh" "$OUTPUT_DIR/lsif-clang"
 chown "$USER:$(id -g)" "$OUTPUT_DIR"/*
 
 # Now create our tarball
-tar zcf "${TARBALL_NAME}" "$OUTPUT_TARBALL_BASENAME"
-echo "Created: ${TARBALL_NAME}"
+pushd "$(dirname "$OUTPUT_DIR")"
+tar zcf "$(basename "$TARBALL_PATH")" "$(basename "$OUTPUT_DIR")"
+popd
+
+OUTPUT_TARBALL_PATH="${OUTPUT_TARBALL_PATH:-}"
+if [ ! -z "$OUTPUT_TARBALL_PATH" ]; then
+  mv "$TARBALL_PATH" "$OUTPUT_TARBALL_PATH"
+  TARBALL_PATH="$OUTPUT_TARBALL_PATH"
+fi
+echo "Created: $TARBALL_PATH"
